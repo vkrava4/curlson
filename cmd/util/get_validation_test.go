8eabfa5f43a3c96f694561ef2bd88e686b1dd566
vkrava4 <vkrava4@gmail.com>
2020-04-18 02:06:01 +0200

    13: GET Validation refactoring: added 'requests per thread' to validator

--- cmd/util/get_validation_test.go
+++ cmd/util/get_validation_test.go
@@ -1,10 +1,12 @@
 package util
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
-func TestGetValidatorForPositiveThreadsWithOKOtherFlags(t *testing.T) {
+func TestValidatePositiveThreadsWithOKOtherFlags(t *testing.T) {
 	var givenPositiveThreads = 999
 	var givenPositiveRequestCount = 999
 	var getValidator = &GetValidator{}
@@ -13,6 +15,71 @@ func TestGetValidatorForPositiveThreadsWithOKOtherFlags(t *testing.T) {
 	var actualValidationResult = validatorEntity.Validate()
 
 	if !actualValidationResult.valid || len(actualValidationResult.messages) > 0 {
+		t.Errorf("Unexpected validation result %v", actualValidationResult)
+	}
+}
+
+func TestValidateNegativeThreadsWithOKOtherFlags(t *testing.T) {
+	var givenZeroThreads = -1
+	var givenPositiveRequestCount = 999
+	var getValidator = &GetValidator{}
+	var validatorEntity = getValidator.AddRequestCount(givenPositiveRequestCount).AddThreads(givenZeroThreads).Entity()
+
+	var actualValidationResult = validatorEntity.Validate()
+
+	if actualValidationResult.valid || strings.Join(actualValidationResult.messages, ",") != fmt.Sprintf(MsgShouldBePositive, "threads", givenZeroThreads) {
+		t.Errorf("Unexpected validation result %v", actualValidationResult)
+	}
+}
+
+func TestValidateZeroThreadsWithOKOtherFlags(t *testing.T) {
+	var givenZeroThreads = 0
+	var givenPositiveRequestCount = 999
+	var getValidator = &GetValidator{}
+	var validatorEntity = getValidator.AddRequestCount(givenPositiveRequestCount).AddThreads(givenZeroThreads).Entity()
+
+	var actualValidationResult = validatorEntity.Validate()
+
+	if actualValidationResult.valid || strings.Join(actualValidationResult.messages, ",") != fmt.Sprintf(MsgShouldBePositive, "threads", givenZeroThreads) {
+		t.Errorf("Unexpected validation result %v", actualValidationResult)
+	}
+}
+
+func TestValidatePositiveRequestCountWithOKOtherFlags(t *testing.T) {
+	var givenPositiveThreads = 999
+	var givenPositiveRequestCount = 1
+	var getValidator = &GetValidator{}
+	var validatorEntity = getValidator.AddRequestCount(givenPositiveRequestCount).AddThreads(givenPositiveThreads).Entity()
+
+	var actualValidationResult = validatorEntity.Validate()
+
+	if !actualValidationResult.valid || len(actualValidationResult.messages) > 0 {
+		t.Errorf("Unexpected validation result %v", actualValidationResult)
+	}
+}
+
+func TestValidateZeroRequestCountWithOKOtherFlags(t *testing.T) {
+	var givenPositiveThreads = 999
+	var givenZeroRequestCount = 0
+	var getValidator = &GetValidator{}
+	var validatorEntity = getValidator.AddRequestCount(givenZeroRequestCount).AddThreads(givenPositiveThreads).Entity()
+
+	var actualValidationResult = validatorEntity.Validate()
+
+	if actualValidationResult.valid || strings.Join(actualValidationResult.messages, ",") != fmt.Sprintf(MsgShouldBePositive, "requests per thread", givenZeroRequestCount) {
+		t.Errorf("Unexpected validation result %v", actualValidationResult)
+	}
+}
+
+func TestValidateNegativeRequestCountWithOKOtherFlags(t *testing.T) {
+	var givenPositiveThreads = 999
+	var givenNegativeRequestCount = -1
+	var getValidator = &GetValidator{}
+	var validatorEntity = getValidator.AddRequestCount(givenNegativeRequestCount).AddThreads(givenPositiveThreads).Entity()
+
+	var actualValidationResult = validatorEntity.Validate()
+
+	if actualValidationResult.valid || strings.Join(actualValidationResult.messages, ",") != fmt.Sprintf(MsgShouldBePositive, "requests per thread", givenNegativeRequestCount) {
 		t.Errorf("Unexpected validation result %v", actualValidationResult)
 	}
 }

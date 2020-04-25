4f2aec1564ea4ddc60daebf8e4c8032652d1236b
vkrava4 <vkrava4@gmail.com>
2020-04-25 23:21:06 +0200

    #24 Tests improvements

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/VividCortex/ewma v1.1.1 // indirect
 	github.com/fatih/color v1.9.0
+	github.com/golang/mock v1.4.3 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/cobra v1.0.0
--- util/logs_test.go
+++ util/logs_test.go
@@ -1,7 +1,99 @@
 package util
 
-//func TestSetupLogs(t *testing.T) {
-//	var config = &app.LogConfiguration{
-//
-//	}
-//}
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/vkrava4/curlson/app"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestSetupLogs(t *testing.T) {
+	var givenConfig = &app.LogConfiguration{
+		Enabled: false,
+		Persist: false,
+		Verbose: false,
+		Log:     logrus.New(),
+		Logfile: nil,
+	}
+
+	var errSetup = SetupLogs(givenConfig)
+	defer cleanup(givenConfig.Logfile.Name())
+
+	if errSetup != nil {
+		t.Errorf("Result is incorrect. An error is not expected: %v", givenConfig)
+	}
+
+	if givenConfig.Logfile == nil || !strings.Contains(givenConfig.Logfile.Name(), "current-execution-") ||
+		!givenConfig.Enabled || givenConfig.Log.Out == nil {
+		t.Errorf("Result is incorrect: %v", givenConfig)
+	}
+
+}
+
+func TestShutdownLogsAndRename(t *testing.T) {
+	var givenConfig = &app.LogConfiguration{
+		Enabled: false,
+		Persist: true,
+		Verbose: false,
+		Log:     logrus.New(),
+		Logfile: nil,
+	}
+
+	var errSetup = SetupLogs(givenConfig)
+	defer cleanup(givenConfig.Logfile.Name())
+
+	if errSetup != nil {
+		t.Errorf("Result is incorrect. An error is not expected: %v", givenConfig)
+	}
+
+	var logfileNameToBeRenamed = givenConfig.Logfile.Name()
+	ShutdownLogs(givenConfig)
+
+	if givenConfig.Logfile != nil || givenConfig.Enabled || givenConfig.Log != nil {
+		t.Errorf("Result is incorrect: %v", givenConfig)
+	}
+
+	var _, infoErr1 = os.Stat(logfileNameToBeRenamed)
+	if !os.IsNotExist(infoErr1) {
+		t.Errorf("File should be renamed: %s", logfileNameToBeRenamed)
+	}
+
+	var info2, infoErr2 = os.Stat(strings.Replace(logfileNameToBeRenamed, "current-execution-", "execution-", -1))
+	if infoErr2 != nil || info2.IsDir() {
+		t.Errorf("File should be renamed: %s", logfileNameToBeRenamed)
+	}
+}
+
+func TestShutdownLogsAndRemove(t *testing.T) {
+	var givenConfig = &app.LogConfiguration{
+		Enabled: false,
+		Persist: false,
+		Verbose: false,
+		Log:     logrus.New(),
+		Logfile: nil,
+	}
+
+	var errSetup = SetupLogs(givenConfig)
+	defer cleanup(givenConfig.Logfile.Name())
+
+	if errSetup != nil {
+		t.Errorf("TestShutdownLogs result is incorrect. An error is not expected: %v", givenConfig)
+	}
+
+	var logfileNameToBeRemoved = givenConfig.Logfile.Name()
+	ShutdownLogs(givenConfig)
+
+	if givenConfig.Logfile != nil || givenConfig.Enabled || givenConfig.Log != nil {
+		t.Errorf("TestShutdownLogsAndRemove is incorrect: %v", givenConfig)
+	}
+
+	var _, infoErr = os.Stat(logfileNameToBeRemoved)
+	if !os.IsNotExist(infoErr) {
+		t.Errorf("File should be removed: %s", logfileNameToBeRemoved)
+	}
+}
+
+func cleanup(file string) {
+	_ = os.Remove(file)
+}
